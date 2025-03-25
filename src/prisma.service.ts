d1d280111c30a93406a7c639acfd689b31409413
Harold Quispe <128565012+HaroldQn@users.noreply.github.com>
2025-03-25 12:49:57 -0500

    Update changes Prisma getAll - Pagination

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,10 +1,13 @@
 
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ProductsService');
+
   async onModuleInit() {
     await this.$connect();
-  }
+    this.logger.log('Database connected');}
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma.service';
+import { PaginationDto } from 'src/common/dto';
 
 @Injectable()
 export class ProductsService {
@@ -18,10 +19,37 @@ export class ProductsService {
     return product;
   }
 
-  findAll() {
-    const getAllProducts = this.prisma.product.findMany();
-    return getAllProducts;
+  async findAll(paginationDto: PaginationDto) {
+  const { page, limit } = paginationDto;
+
+  // Si no se envían `page` ni `limit`, retornar todos los productos
+  if (!page || !limit) {
+    return {
+      data: await this.prisma.product.findMany(),
+      meta: {
+        total: await this.prisma.product.count(),
+        page: null,
+        lastPage: null,
+      },
+    };
   }
+
+  const totalPages = await this.prisma.product.count();
+  const lastPage = Math.ceil(totalPages / limit);
+
+  return {
+    data: await this.prisma.product.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+    }),
+    meta: {
+      total: totalPages,
+      page: page,
+      lastPage: lastPage,
+    },
+  };
+}
+
 
   findOne(id: number) {
     return `This action returns a #${id} product`;
